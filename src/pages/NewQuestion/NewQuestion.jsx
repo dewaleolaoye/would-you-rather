056654278b850a412b81204d35ab6efa15c878fd
dewaleolaoye <adewaleolaoye21@gmail.com>
2021-08-27 09:28:47 +0100

    feat: New question

--- src/pages/NewQuestion/NewQuestion.jsx
+++ src/pages/NewQuestion/NewQuestion.jsx
@@ -1,7 +1,31 @@
+import style from './NewQuestion.module.scss';
+import Button from '../../components/Button/Button';
+
 const NewQuestion = () => {
   return (
-    <div>
-      <h1>NewQuestion</h1>
+    <div className={style.newQuestion}>
+      <div className={style.header}>
+        <h1>Create NewQuestion</h1>
+      </div>
+
+      <div className={style.content}>
+        <p>Complete this question:</p>
+        <h2>Would you rather...</h2>
+
+        <form action=''>
+          <input type='text' placeholder='Enter Option One Text Here' />
+          <div className={style.divider}>
+            <hr />
+
+            <p>OR</p>
+
+            <hr />
+          </div>
+          <input type='text' placeholder='Enter Option Two Text Here' />
+
+          <Button className={style.btn} name='Submit' />
+        </form>
+      </div>
     </div>
   );
 };
