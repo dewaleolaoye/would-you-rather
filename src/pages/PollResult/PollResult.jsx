b6ff49a15eca368174d7be6651e707b9454db86b
dewaleolaoye <adewaleolaoye21@gmail.com>
2021-08-27 10:40:49 +0100

    feat: Poll result

--- src/pages/PollResult/PollResult.jsx
+++ src/pages/PollResult/PollResult.jsx
@@ -1,24 +1,37 @@
 import PollResultCard from '../../components/PollResultCard/PollResultCard';
+import style from './PollResult.module.scss';
+import faker from 'faker';
 
 const PollResult = () => {
-  return (
-    <div>
-      <h1>Poll Result</h1>
-      <PollResultCard percentage={20} vote={true} />
-      <br />
-      <PollResultCard percentage={50} />
+  const avatar = faker.image.avatar();
 
-      <br />
-      <br />
+  return (
+    <>
+      <div className={style.wrapper}>
+        <h1>Asked by Adewale Olaoye</h1>
+
+        <div className={style.result}>
+          <div className={style.avatar}>
+            <img src={avatar} alt='leaderboard' />
+          </div>
+
+          <div className={style.details}>
+            <PollResultCard percentage={20} vote={true} />
+            <br />
+            <PollResultCard percentage={50} />
+          </div>
+        </div>
+      </div>
 
       <a
         target='_blank'
         rel='noreferrer'
         href='https://icons8.com/icon/80776/trophy'
+        style={{ marginTop: '3rem', display: 'block' }}
       >
         Trophy icon by Icons8
       </a>
-    </div>
+    </>
   );
 };
 
