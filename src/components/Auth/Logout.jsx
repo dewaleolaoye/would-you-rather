0d917b1a4f42181ef7da235e10cbcfb5a6612878
dewaleolaoye <adewaleolaoye21@gmail.com>
2021-08-29 03:14:35 +0100

    chore: Delete unused files

--- src/components/Auth/Logout.jsx
+++
@@ -1,9 +0,0 @@
-const Logout = () => {
-  return (
-    <div>
-      <h1>Logout</h1>
-    </div>
-  );
-};
-
-export default Logout;
--- src/components/Auth/Signin.jsx
+++
@@ -1,9 +0,0 @@
-const Signin = () => {
-  return (
-    <div>
-      <h1>Signin</h1>
-    </div>
-  );
-};
-
-export default Signin;
